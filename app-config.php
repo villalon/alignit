d0ad485c47182c748e078f81bb8fc8a4a9c36b75
Carlos Bravo <carlos@MacBook-Pro-de-Carlos.local>
2016-04-05 13:37:42 -0300

    Bugfixes varios + Bootstrap

---
+++ app-config.php
@@ -0,0 +1,9 @@
+<?php
+
+// ** MySQL settings - You can get this info from your web host ** //
+
+define('DB_NAME', 'alignit');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '12345678');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
